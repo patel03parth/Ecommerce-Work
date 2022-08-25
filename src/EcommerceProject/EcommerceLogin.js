818829627c5adcbf71469b10676e32a45cc65b88
patel033parth <patel033parth@gmail.com>
2022-08-26 02:18:43 +0530

    commited

---
+++ src/EcommerceProject/EcommerceLogin.js
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import './CSS/Login.css'
+
+export default function EcommerceLogin() {
+
+    const [data, setData] = useState({
+
+        email: "",
+        password: "",
+
+    })
+
+    const changSet = (e) => {
+
+        setData({ ...data, [e.target.name]: e.target.value });
+    }
+
+    const mySaveData = (e) => {
+
+        e.preventDefault();
+
+        axios.post("http://localhost:8002/api/auth/login", data,{
+        
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+                "Authorization": `Bearer` 
+            }})
+            .then(y => {
+
+                console.log(y)
+                localStorage.setItem("data", JSON.stringify(y.data));
+            
+            })
+        console.log(data);
+    }
+
+
+    return (
+        <div className='Bg'>
+
+            <form className='card' onSubmit={mySaveData}>
+
+                <h1>Sign In</h1>
+
+                <div className="col-auto">
+                    <label className="col-form-label">Email</label>
+                </div>
+                <div className="col-auto">
+                    <input type="email" className="form-control" name='email' onChange={changSet} />
+                </div>
+
+                <div className="col-auto">
+                    <label className="col-form-label">Password</label>
+                </div>
+                <div className="col-auto">
+                    <input type="password" className="form-control" name='password' onChange={changSet} />
+                </div>
+
+                <div className='button m-5'>
+                    <button className='bt'>Login</button>
+                </div>
+
+                <div className='row1'>
+
+                <p className='me-2'>Don't have Account?</p>
+                <a href='#!'>Sign Up</a>
+                    
+                </div>
+
+            </form>
+        </div>
+    )
+}
---
+++ src/EcommerceProject/EcommerceRegistration.js
@@ -0,0 +1,130 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import "./CSS/Regi.css"
+
+export default function EcommerceRegistration() {
+
+    const [data, setData] = useState({
+        username: "",
+        name: "",
+        email: "",
+        password: "",
+        confirmpassword: "",
+        role: "",
+        phone: "",
+        address: "",
+        companyName: "",
+        image: ""
+
+    })
+
+    const changSet = (e) => {
+
+        if (e.target.type == "file")
+            setData({ ...data, [e.target.name]: e.target.files[0] });
+        else
+            setData({ ...data, [e.target.name]: e.target.value });
+    }
+
+    const mySaveData = (e) => {
+
+        e.preventDefault();
+        var formData = new FormData();
+
+        formData.append("username", data.username);
+        formData.append("name", data.name);
+        formData.append("email", data.email);
+        formData.append("password", data.password)
+        formData.append("confirmpassword", data.passwordConfirmation);
+        formData.append("role", data.role);
+        formData.append("phone", data.phone);
+        formData.append("address", data.address)
+        formData.append("companyName", data.companyName);
+        formData.append("image", data.image);
+
+
+
+        axios.post("http://localhost:8002/api/auth/register", formData).then(y => {
+
+            console.log(y);
+
+        })
+    }
+
+
+    return (
+        <div className='Bg1'>
+
+            <form className='card1' onSubmit={mySaveData}>
+
+                <h1>Sign Up</h1>
+
+                <div className='d-flex'>
+
+                    <div className="col-auto">
+                        <input type="text" className="form-control" name='username' placeholder='UserName' onChange={changSet} />
+                    </div>
+
+                    <div className="col-auto">
+                        <input type="text" className="form-control" name='name' placeholder='Name' onChange={changSet} />
+                    </div>
+
+                </div>
+
+                <div className='d-flex'>
+
+                    <div className="col-auto">
+
+                        <input type="email" className="form-control" name='email' placeholder='Email' onChange={changSet} />
+                    </div>
+
+                    <div className="col-auto">
+                        <input type="password" className="form-control" name='password' placeholder='Password' onChange={changSet} />
+                    </div>
+
+                    <div className="col-auto">
+                        <input type="password" className="form-control" name='confirmpassword' placeholder='ConfirmPassword' onChange={changSet} />
+                    </div>
+
+                    <select className='select px-2' name='role'>
+                        <option value='user'>User</option>
+                        <option value='seller'>Seller</option>
+                    </select>
+
+                </div>
+
+                <div className='d-flex'>
+
+                    <div className="col-auto">
+                        <input type="password" className="form-control" placeholder='Phone' name='phone' onChange={changSet} />
+                    </div>
+
+                    <div className="col-auto">
+                        <input type="password" className="form-control" name='address' placeholder='Address' onChange={changSet} />
+                    </div>
+
+                    <div className="col-auto">
+                        <input type="password" className="form-control" name='companyName' placeholder='Company Name' onChange={changSet} />
+                    </div>
+
+                </div>
+
+                <div className="col-auto">
+                    <input type="file" className="form-control" name='image' onChange={changSet} />
+                </div>
+
+                <div className='button1 mx-auto my-4'>
+                    <button type="button" className='bt1'>Register</button>
+                </div>
+
+                <div className='row1'>
+
+                    <p className='me-2'>Already have an Account?</p>
+                    <a href='#!'>Sign In</a>
+
+                </div>
+            </form>
+
+        </div>
+    )
+}
